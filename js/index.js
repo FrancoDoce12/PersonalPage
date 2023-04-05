3fe4ef30f7d08a232ffcf4a1bc7fc804f9e9e66f
Franco Sanchez Alejandro <francosanche@gmail.com>
2023-04-05 09:48:07 -0300

    drop box added

--- js/index.js
+++ js/index.js
@@ -21,3 +21,34 @@ const elemenst_to_animate = document.querySelectorAll(
 elemenst_to_animate.forEach((el) => {
     observer.observe(el)
 })
+
+// drop dawn code
+
+const isDroped = {value:false}
+
+const dropdawnMenu = document.querySelector(".dropdownMenu")
+const dropdawnButton = document.querySelector("#dropdownButton")
+
+dropdawnButton.addEventListener("click", () =>{
+    if (isDroped.value){
+        // close the dropbox
+        dropdawnMenu.classList.remove("dropdownMenu-show")
+    } else {
+        // open the dropbox
+        dropdawnMenu.classList.add("dropdownMenu-show")
+    }
+    isDroped.value = !isDroped.value
+
+})
+
+document.addEventListener('click', (event) => {
+    if (!dropdawnButton.contains(event.target)) {
+        // The click occurred outside the container element
+        isDroped.value = false
+        // close the dropbox
+        dropdawnMenu.classList.remove("dropdownMenu-show")
+        console.log('Click outside container');
+    }
+});
+
+
